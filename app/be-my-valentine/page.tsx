96a98390ff00e54a71ef688828ea1569ad20b2fb
Andrew Tran <andrewdaotran@gmail.com>
2024-02-07 08:30:29 -0800

    Add more button content and movement

--- app/be-my-valentine/page.tsx
+++ app/be-my-valentine/page.tsx
@@ -17,6 +17,8 @@ const BeMyValentine = () => {
 		'...',
 		'now you gotta chase me...',
 		'you hate me...',
+		"you're evil",
+		'fine...',
 	])
 
 	const [noButtonSize, setNoButtonSize] = useState([
@@ -25,9 +27,11 @@ const BeMyValentine = () => {
 		'py-[0.5rem] px-3 text-xs',
 		'py-[0.5rem] px-2 text-xs',
 		'py-[0.5rem] px-2 text-xs absolute -top-5 right-10 ',
-		' px-1 text-xs absolute -bottom-2 left-10 ',
-		'px-1 text-xs absolute -top-3 left-[50%] transform -translate-x-1/2 ',
-		'px-1 text-xs absolute  -right-12 top-16 transform  rotate-90',
+		'px-1 text-xs absolute -bottom-2 left-10',
+		'px-1 text-[10px] absolute transform left-10 -top-[0.65rem] ',
+		'px-1 text-[8px] absolute  transform  rotate-90 top-[50%] -translate-x-1/2 -right-[4.2rem]  ',
+		'px-1 text-[8px] absolute  transform  top-0 right-0 z-20',
+		'px-1 text-[6px] absolute  transform  top-[50%] rotate-90 -left-3  z-20',
 	])
 
 	const [yesButtonSize, setYesButtonSize] = useState([
@@ -37,8 +41,10 @@ const BeMyValentine = () => {
 		'py-8 px-16 text-2xl',
 		'py-10 px-20 text-3xl z-10',
 		'py-12 px-24 text-4xl z-10',
-		'py-14 px-28 text-5xl ',
-		'py-16 px-32 text-6xl ',
+		'py-14 px-28 text-5xl z-10',
+		'py-16 px-32 text-6xl z-10',
+		'py-[4.5rem] px-36 text-7xl z-10',
+		'py-20 px-40 text-8xl z-10',
 	])
 
 	const noButtonHandler = () => {
@@ -51,24 +57,26 @@ const BeMyValentine = () => {
 
 	return (
 		<div className='relative grid justify-items-center items-center h-screen'>
-			<div className='border border-black p-8 grid gap-8 relative z-10 bg-inherit shadow-lg'>
-				<h2 className='text-3xl text-center'>Will you be my Valentine?</h2>
-				<div className='flex gap-10  justify-center items-center relative'>
-					<button
-						className={`border border-black rounded-md bg-green-300  ${yesButtonSize[noClickedCounter]} `}
-					>
-						Yes!
-					</button>
-					{noClickedCounter <= 5 && (
+			<div className='relative'>
+				<div className='border border-black p-8 grid gap-8 z-10 bg-white shadow-lg relative'>
+					<h2 className='text-3xl text-center '>Will you be my Valentine?</h2>
+					<div className='flex gap-10  justify-center items-center relative -translate-y'>
 						<button
-							className={`border border-black rounded-md  bg-red-400  ${noButtonSize[noClickedCounter]}`}
-							onClick={noButtonHandler}
+							className={`border border-black rounded-md bg-green-300   ${yesButtonSize[noClickedCounter]} `}
 						>
-							{noClickedButtonContent[noClickedCounter]}
+							Yes!
 						</button>
-					)}
+						{noClickedCounter <= 5 && (
+							<button
+								className={`border border-black rounded-md  bg-red-400  ${noButtonSize[noClickedCounter]}`}
+								onClick={noButtonHandler}
+							>
+								{noClickedButtonContent[noClickedCounter]}
+							</button>
+						)}
+					</div>
 				</div>
-				{noClickedCounter > 5 && (
+				{noClickedCounter > 5 && noClickedCounter <= 7 && (
 					<button
 						className={`border border-black rounded-md  bg-red-400  ${noButtonSize[noClickedCounter]}`}
 						onClick={noButtonHandler}
@@ -77,6 +85,15 @@ const BeMyValentine = () => {
 					</button>
 				)}
 			</div>
+			{noClickedCounter > 7 && (
+				<button
+					className={`border border-black rounded-md  bg-red-400  ${noButtonSize[noClickedCounter]}`}
+					onClick={noButtonHandler}
+				>
+					{noClickedButtonContent[noClickedCounter]}
+				</button>
+			)}
+
 			<Footer />
 		</div>
 	)
